d49e4f147ba2110605423bdd5c2bddd931db057e
SANTIAGO NICOLÁS HONG <49189314@ort>
2025-10-23 15:26:38 -0300

    Algunas Views

--- obj/Debug/net8.0/EcoPlay.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcoPlay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd481785dc28cc5792933e4f51d34ca5756e03e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eaeebe87b10e9725827b9cb64f6fe73317c7ee37")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
