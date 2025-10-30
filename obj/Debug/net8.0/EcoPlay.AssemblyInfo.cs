7f86c710d290f58444a1a6b4d341426bb82a975d
NICOLÁS JAIR JAJURIN <48861075@ort>
2025-10-30 15:53:56 -0300

    fin hackaton

--- obj/Debug/net8.0/EcoPlay.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcoPlay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02c9fb04c9b2d1e1bd4c3f00c05a2033c50ed9b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1970ebc3b45dbf63cc253ff72a229d6de39260d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
