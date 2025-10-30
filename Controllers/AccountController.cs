c33db85c363f5b8411172ff5d561cc3b09414bab
GAEL JONAS ZOLOTOW <49379479@ort>
2025-10-30 11:59:59 -0300

    antes de almorzar

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,17 +1,66 @@
 using Microsoft.AspNetCore.Mvc;
+using EcoPlay.Models;
 
 namespace EcoPlay.Controllers
 {
     public class AccountController : Controller
     {
+        // GET: /Account/IniciarSesion
         public IActionResult IniciarSesion()
         {
             return View();
         }
 
+        // POST: /Account/IniciarSesion
+        [HttpPost]
+        public IActionResult IniciarSesion(string usuario, string contraseña)
+        {
+            var user = BD.Login(usuario, contraseña);
+
+            if (user != null)
+            {
+                // Si el login es correcto, redirigimos al Home
+                // (Podés cambiarlo por otra vista o página de bienvenida)
+                ViewBag.Mensaje = "Inicio de sesión exitoso. ¡Bienvenido " + user.Username + "!";
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Si falla, mostramos un mensaje de error
+                ViewBag.Error = "Usuario o contraseña incorrectos.";
+                return View();
+            }
+        }
+
+        // GET: /Account/Registrarse
         public IActionResult Registrarse()
         {
             return View();
+        }
+
+        // POST: /Account/Registrarse
+        [HttpPost]
+        public IActionResult Registrarse(string username, string email, string password, string fecha)
+        {
+            DateTime fechaNacimiento;
+            if (!DateTime.TryParse(fecha, out fechaNacimiento))
+            {
+                ViewBag.Error = "La fecha no tiene un formato válido (dd/mm/aa).";
+                return View();
+            }
+
+            bool registro = BD.Registrarse(username, email, fechaNacimiento, password, "default.png");
+
+            if (registro)
+            {
+                // Si se registra bien, lo mandamos al login
+                return RedirectToAction("IniciarSesion");
+            }
+            else
+            {
+                ViewBag.Error = "El nombre de usuario ya existe. Elija otro.";
+                return View();
+            }
         }
     }
 }
--- obj/Debug/net8.0/EcoPlay.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcoPlay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f5aa71a2af6af8b9b1167aecd7d987132f16d9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28642d2cbfd02963ba1929f0a07f40e5cf225771")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
