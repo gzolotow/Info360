12a9e7b27e6974cf05fc9d2de6259440b0f59c09
SANTIAGO NICOLÁS HONG <49189314@ort>
2025-11-05 12:15:00 -0300

    css muy avanzado

--- obj/Debug/net8.0/EcoPlay.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcoPlay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1970ebc3b45dbf63cc253ff72a229d6de39260d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dc1e2aaa6578e855aac4fa1f0a06cdb68063043")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
