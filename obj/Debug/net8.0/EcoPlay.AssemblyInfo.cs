e2937537177ca4945264f4764536ac32510537fc
SANTIAGO NICOLÁS HONG <49189314@ort>
2025-11-12 12:16:48 -0300

    Mejorando el home y datos en la base

--- obj/Debug/net8.0/EcoPlay.AssemblyInfo.cs
+++ obj/Debug/net8.0/EcoPlay.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c40a7463e383362bbf0d76cdefa28c82bdb5ccb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f35d9167128831f6444f01f2b760afe4e8b0f1f")]
 [assembly: System.Reflection.AssemblyProductAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EcoPlay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
